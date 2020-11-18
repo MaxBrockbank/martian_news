de19daf6a71ebf31d3d2a1ce7e87c0f10a964563
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-18 10:30:01 -0800

    log JSON data from NASA API to console to confirm we recieve it in the right format
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/js/marsWeather.js
+++ src/js/marsWeather.js
@@ -1,7 +1,7 @@
 export default class MarsWeatherService {
   static async getWeatherData(){
     try{
-      const response = await fetch(`https://api.nasa.gov/oinsight_weather/>api_key=${process.env.NASA_KEY}&feedtype=json&ver=1.0`);
+      const response = await fetch(`https://api.nasa.gov/insight_weather/?api_key=${process.env.NASA_KEY}&feedtype=json&ver=1.0`);
       if(!response.ok){
         throw Error(response.statusText);
       }
--- src/main.js
+++ src/main.js
@@ -2,3 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../src/css/styles.css';
+import MarsWeatherService from './../src/js/marsWeather.js'
+$
+/*
+let mars = MarsWeatherService.getWeatherData();
+console.log(mars); */
+
+function getElements(response) {
+  console.log(response);
+}
+
+async function makeApiCall() {
+  const response = await MarsWeatherService.getWeatherData();
+  getElements(response);
+}
+
+makeApiCall();
+
