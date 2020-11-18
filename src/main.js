0ffa65deac40791123a3cb47ac2eeea75ac4de0e
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-18 11:20:39 -0800

    properly filter sols with all the data we need
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/main.js
+++ src/main.js
@@ -9,7 +9,13 @@ let mars = MarsWeatherService.getWeatherData();
 console.log(mars); */
 
 function getElements(response) {
-  console.log(response);
+  const responseArray = Object.entries(response);
+  console.log(responseArray);
+  const solData = responseArray.filter( sol => sol[1].AT);
+  console.log(solData);
+  // const{"698":sol698, "699":sol699, "700":sol700} = response;
+  // console.log(sol698, sol699, sol700);
+
 }
 
 async function makeApiCall() {
